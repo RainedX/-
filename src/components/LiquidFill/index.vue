9c3327a729cc4247cccde1ea71c24835e78b07ca
RainedX <1355247497@qq.com>
2021-06-11 14:32:54 +0800

    feat：新增词云图

--- src/components/LiquidFill/index.vue
+++ src/components/LiquidFill/index.vue
@@ -1,11 +1,72 @@
 <template>
-  <div>LiquidFill</div>
+  <ve-liquidfill
+    :data="chartData"
+    height="100%"
+    :settings="chartSettings"
+  ></ve-liquidfill>
 </template>
 
 <script>
+function getColor(value) {
+  return value > 0 && value <= 0.5 ? 'rgba(97,216,0,.7)'
+      : value > 0.5 && value <= 0.8 ? 'rgba(204,178,26,.7)'
+        : value > 0.8 ? 'rgba(241,47,28,.7)' : '#c7c7cb'
+}
+
 export default {
   data() {
-    return {}
-  }
-}
+    return {
+      chartData: {
+        columns: ['title', 'percent'],
+        rows: [
+          {
+            title: 'rate',
+            percent: 0.38,
+          },
+        ],
+      },
+      chartSettings: {},
+    };
+  },
+  mounted() {
+    this.chartSettings = {
+      seriesMap: {
+        rate: {
+          radius: '80%',
+          label: {
+            normal: {
+              formatter(v) {
+                return `${Math.floor(v.data.value * 100)}%`;
+              },
+              textStyle: {
+                fontSize: 36,
+                color: '#999',
+                fontWeight: 'normal',
+              },
+            },
+          },
+          outline: {
+            itemStyle: {
+              borderColor: '#aaa4a4',
+              borderWidth: 1,
+              color: 'none',
+              shadowBlur: 0,
+              shadowColor: '#fff',
+            },
+            borderDistance: 0,
+          },
+          backgroundStyle: {
+            color: '#fff',
+          },
+          itemStyle: {
+            shadowBlur: 0,
+            shadowColor: '#fff',
+          },
+          amplitude: 8,
+          color: [getColor(this.chartData.rows[0].percent)],
+        },
+      },
+    };
+  },
+};
 </script>
--- src/components/WordCloud/index.vue
+++ src/components/WordCloud/index.vue
@@ -1,11 +1,40 @@
 <template>
-  <div>WordCloud</div>
+  <ve-wordcloud :data="chartData" height="100%" :settings="chartSettings"></ve-wordcloud>
 </template>
 
 <script>
 export default {
   data() {
-    return {}
+    return {
+      chartData: {
+        columns: ['name', 'value'],
+        rows: [
+          {
+            name: 'visualMap',
+            value: 2199
+          },
+          {
+            name: 'rained',
+            value: 3399
+          },
+          {
+            name: 'yy',
+            value: 1922
+          },
+          {
+            name: 'hj',
+            value: 2139
+          },
+          {
+            name: 'dd',
+            value: 4439
+          }
+        ]
+      },
+      chartSettings: {
+          color: ['rgba(97,216,0,.7)', 'rgba(204,178,26,.7)', 'rgba(245,166,35,.7)', 'rgba(156,13,113,.7)']
+        }
+    }
   }
 }
 </script>
