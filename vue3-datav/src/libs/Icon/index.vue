1d6d674817502904a9b414943f0ddf4c91e29bab
RainedX <1355247497@qq.com>
2021-06-21 10:21:33 +0800

    feat:自定义svg

---
+++ vue3-datav/src/libs/Icon/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="icon-wrapper">
+    <svg class="icon">
+      <use :href="iconName"></use>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+  props: {
+    name: String,
+    prefix: {
+      type: String,
+      default: 'icon'
+    }
+  },
+  setup(ctx) {
+    const iconName = `#${ctx.prefix}${ctx.name}`;
+    return {
+      iconName
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.icon-wrapper {
+  display: inline-block;
+}
+.icon {
+  width: 100%;
+  height: 100%;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
--- vue3-datav/src/main.js
+++ vue3-datav/src/main.js
@@ -1,6 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import Icon from './libs/Icon/index';
+createApp(App)
+  .use(Icon)
+  .use(store)
+  .use(router)
+  .mount('#app');
